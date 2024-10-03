3d12468c13944a32b1efaa668dcac37b8eb3e065
Alexis <ahuamani@unjbg.edu.pe>
2024-10-03 17:00:55 -0500

    VERSION2

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     var num1:EditText?=null
     var num2:EditText?=null
     var result:TextView?=null
+
+    var texto:EditText?=null
+    var restexto:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         num1=findViewById(R.id.Numero1)
         num2=findViewById(R.id.Numero2)
         result=findViewById(R.id.Resultado)
+
+        texto=findViewById(R.id.palabra)
+        restexto=findViewById(R.id.respalabra)
     }
     fun suma(view: View){
         val aux1string=num1?.text.toString()
@@ -31,5 +38,28 @@ class MainActivity : AppCompatActivity() {
 
         val resultadoint=aux1int+aux2int
         result?.text=resultadoint.toString()
+    }
+
+    fun palabra(view: View){
+        var palabra=texto?.text.toString()
+        palabra=palabra.toLowerCase()
+        palabra=palabra.replace("\\s".toRegex(), replacement = "")
+        val longitud=palabra.length
+        var cont=0
+        var igual=true
+
+        for (i in longitud-1 downTo 0){
+            if(palabra[i]!=palabra[cont]){
+                igual=false
+                break
+            }
+            cont++
+        }
+        if(igual==true){
+            restexto?.text="Si es un palindromo"
+        }
+        else{
+            restexto?.text="No es palindromo"
+        }
     }
 }
